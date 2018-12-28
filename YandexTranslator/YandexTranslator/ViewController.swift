64e4f8374850ae5f782c3fc23386ee12ec71d238
Tumanov Nikita <2manov97@gmail.com>
2018-12-28 15:43:26 +0300

    add project

---
+++ YandexTranslator/YandexTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YandexTranslator
+//
+//  Created by Никита Туманов on 28/12/2018.
+//  Copyright © 2018 Никита Туманов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
